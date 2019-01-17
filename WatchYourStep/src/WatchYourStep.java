de230b7f9ed00e0f3de2d7123fc31630519f9f64
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2019-01-17 15:03:34 -0500

    Task 1 Step 3

--- WatchYourStep/src/WatchYourStep.java
+++ WatchYourStep/src/WatchYourStep.java
@@ -1,4 +1,10 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class WatchYourStep extends JFrame {
 
@@ -10,6 +16,24 @@ public class WatchYourStep extends JFrame {
 		pack(); 
 		setLocationRelativeTo(null);  
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void initGUI() { 
+		//TITLE PANEL 
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBackground(Color.BLACK);
+		add(titlePanel, BorderLayout.CENTER);
+		
+		//TITLE LABEL
+		JLabel titleLabel = new JLabel(" Watch Your Step ");
+		Font titleFont = new  Font ("Times New Roman", Font.BOLD, 50);
+		titleLabel.setFont(titleFont);
+		titleLabel.setForeground(Color.WHITE);
+		titleLabel.setBackground(Color.BLUE);
+		titleLabel.setOpaque(true);
+		//add label to window
+		add(titleLabel, BorderLayout.PAGE_START);
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
 	}
 
 	public static void main(String[] args) {
